1d4ceb165b17661b6c095947d8e6d850a9f1eabc
Manoj-kr <mroul914@gmail.com>
2025-03-11 09:54:40 +0530

    First commit

---
+++ src/main/java/com/psa/PsaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.psa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsaPracticeApplication.class, args);
+	}
+
+}
